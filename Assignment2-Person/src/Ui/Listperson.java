020a93e9a845be54db8a5cb7c585048659a903d3
Ramshankar-Bhuvaneswaran <156970038+Ramshankar-Bhuvaneswaran@users.noreply.github.com>
2024-02-14 19:55:50 -0500

    Add files via upload

--- Assignment2-Person/src/Ui/Listperson.java
+++ Assignment2-Person/src/Ui/Listperson.java
@@ -8,6 +8,7 @@ import java.awt.CardLayout;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.table.DefaultTableModel;
+import person.Address;
 import person.Person;
 import person.Persondirectory;
 
@@ -26,7 +27,10 @@ public class Listperson extends javax.swing.JPanel {
         initComponents();
         this.Right= Right;
         this.person= person;
+        prepopulate();
         poputlatePersonDetails();
+        
+        
 
     }
 
@@ -202,6 +206,67 @@ public class Listperson extends javax.swing.JPanel {
 
     }
     }
+
+    private void prepopulate() {
+        DefaultTableModel model=  (DefaultTableModel) jTable1.getModel();
+        model.setRowCount(0);
+        preppop("New York", "NY", "5th Avenue", "1001", "10001",
+                                     "Los Angeles", "CA", "Sunset Boulevard", "101", "90001",
+                                     "John", "Doe", "123456789", "1234567");
+        
+      
+        preppop("Chicago", "IL", "Lake Shore Drive", "202", "60001",
+                                     "Houston", "TX", "Main Street", "303", "77001",
+                                     "Jane", "Smith", "987654321", "9876543");
+        
     
+        preppop("San Francisco", "CA", "Market Street", "404", "94101",
+                                     "Miami", "FL", "Ocean Drive", "505", "33101",
+                                     "Alice", "Johnson", "123987654", "1239876");
+        
+        preppop("Seattle", "WA", "Pine Street", "606", "98101",
+                                     "Atlanta", "GA", "Peachtree Street", "707", "30301",
+                                     "Bob", "Williams", "45689123", "4567891");
+        
+        preppop("Boston", "MA", "Commonwealth Avenue", "808", "02101",
+                                     "Denver", "CO", "Larimer Square", "909", "80201",
+                                     "Carol", "Davis", "789123456", "7891234");        
+//        Object[] r1= {"ram","shankar","chennai","600028","bnglr","20000"};
+//        Object[] r2= {"nick","j","newyork","04028","qubec","42522"};
+//        Object[] r3= {"carl","max","tempe","60028","florida","23500"};
+//        Object[] r4= {"srunith","sai","kochi","60338","bangkok","7377"};
+//        Object[] r5= {"ninja","hattori","beijing","82224","seattle","94880"};
+//        model.addRow(r1);
+//        model.addRow(r2);
+//        model.addRow(r3);
+//        model.addRow(r4);
+//        model.addRow(r5);
+
+    }
+
+    private void preppop(String hcity, String hstate, String hsn, String hunit, String hzip,String wcity, String wstate, String wsn, String wunit, String wzip,String Fn, String Ln, String ssn, String license)
+    {
+           Address address = new Address();
+           Address address1 = new Address();
+           Person Persondirectory = person.addPerson();
+           
+           address.setCity(hcity);
+           address.setState(hstate);
+           address.setStreetName(hsn);
+           address.setUnit(hunit);
+           address.setZipcode(Integer.parseInt(hzip));
+           address1.setCity(wcity);
+           address1.setState(wstate);
+           address1.setStreetName(wsn);
+           address1.setUnit(wunit);
+           address1.setZipcode(Integer.parseInt(wzip));
+           
+           Persondirectory.setFirstName(Fn);
+           Persondirectory.setLastName(Ln);
+           Persondirectory.setSSN(Integer.parseInt(ssn));
+           Persondirectory.setLicense(Integer.parseInt(license));
+           Persondirectory.setHomeaddress(address);
+           Persondirectory.setWorkaddress(address1);    }
+
     
 }
